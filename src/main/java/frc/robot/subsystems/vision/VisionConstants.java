92823536000ec394af42bc0f893c0a45795465aa
audrey <audreypjohnson@gmail.com>
2025-02-09 21:56:07 -0800

    added front camera (arducam-3) and created test path

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -11,8 +11,16 @@ public class VisionConstants {
 
   // index 0 -> arducam-1, etc
   public static final Transform3d[] CAMERA_TRANSFORM = {
-    new Transform3d(0.258, 0, 0.454, new Rotation3d(Math.PI, -0.6109, 0)),
-    new Transform3d(0.302990, -0.260, 0.460722, new Rotation3d( Math.PI, Math.toRadians(35), Math.toRadians(55)))
+    // arducam-1 (front center)
+    // new Transform3d(0.258, 0, 0.454, new Rotation3d(Math.PI, -Math.toRadians(35), 0)),
+    new Transform3d(0.258, 0, 0.454, new Rotation3d(0, -Math.toRadians(35), 0)),
+
+    // arducam-2 (front left)
+    new Transform3d(
+        0.258, 0.260, 0.454, new Rotation3d(0, -Math.toRadians(35), Math.toRadians(55))),
+
+    // arducam-3 (back)
+    new Transform3d(-0.1524, -0.0795, 0.2794, new Rotation3d(Math.PI, -Math.toRadians(160), 0))
   };
 
   public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT =
