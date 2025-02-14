d9747636c0c5806e84bbdf13ccf96f551a421b6e
Bruce <brucebenjaminpeters@gmail.com>
2025-02-13 16:02:16 -0800

    fixed logger error (it wasn't imported :0)

--- src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.superstructure;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
